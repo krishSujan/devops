89b37ebb14647bfb4b47d7b38062384b4d44a1c6
peerclasses <peerclasses@gmail.com>
2020-11-28 20:34:58 +0100

    env.sh update

--- dev/env.sh
+++ dev/env.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+/usr/local/bin/aws ec2 run-instances \
+image=ami-0ba7c4110ca9bfe0b
+insTyp=t2.micro
+subnet=subnet-31a3804a
+SGs=sg-02d28d702be2ae7fc 
+KeyPair=terraform
+count=1
+region=eu-west-3
+
 
 
